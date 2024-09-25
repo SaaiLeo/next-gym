3d59e8ecf64a35778b9d0c46038598558133ea7b
Sai Aik Sei Mouk <saiseimouk.mdy@gmail.com>
2024-09-26 02:11:46 +0700

    add api plan put method

--- app/api/plan/route.js
+++ app/api/plan/route.js
@@ -12,3 +12,8 @@ export async function GET() {
     return Response.json(plans)
 }
 
+export async function PUT(request) {
+    const body = await request.json()
+    const plan = await Plan.findByIdAndUpdate(body._id,body)
+    return Response.json(plan)
+}
