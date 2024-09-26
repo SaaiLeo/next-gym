a89b8cbeadfb613e0a1d7d39f64b066bfb2c5db2
saithaw23 <saithaw619@gmail.com>
2024-09-26 20:56:27 +0700

    Add POST and GET for member api

---
+++ app/api/member/route.js
@@ -0,0 +1,13 @@
+import Member from '@/models/Member'
+
+export async function POST(request) {
+    const body = await request.body
+    const member = new  Member(body)
+    await member.save()
+    return Response.json(member)
+}
+
+export async function GET() {
+    const members = await Member.find().sort({name: 1})
+    return Response.json(members)
+}
